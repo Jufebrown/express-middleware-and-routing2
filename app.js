745e429bac6435b4e2d6650028dd6d4cfd6dbbf4
Jufebrown <jufebrown@gmail.com>
2017-09-07 13:27:23 -0500

    got egg to show up in terminal

--- app.js
+++ app.js
@@ -4,21 +4,35 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+const makeEgg = () => {
+  console.log('        ,ggadddd8888888bbbbaaa,_\n     ,ad888,      `Y88,      `Y888baa,\n   ,dP"  "Y8b,      `"Y8b,      `"Y8888ba,\n  ,88      "Y88b,      `"Y8ba,       `"Y88Ya,\n ,P88b,      `"Y88b,       `"Y8ba,_       ""8a,\n,P\'"Y88b,        "Y88b,        `"Y8ba,_      `Ya,\n8\'    "Y88b,        ""Y8ba,         ""Y8ba,_   `8,\n8b       "Y88b,         ""Y8ba,_         ""Y88baaY\n88b,        "Y88ba,         ""Y88ba,_         `""P\n8Y88ba,        ""Y88ba,_         ""Y88ba,,_    ,P\'\n`b,"Y88ba,         ""Y88baa,_         """Y888bd"\n `b, `"Y88ba,_         ""Y888baa,_         ,8"\n  `8,   `""Y88ba,_         `"""Y8888baaaaaP"\n   `Ya,     `""Y888ba,_           `"d88P"\n     `"Yb,,_     `""Y888baa,__,,adP""\'\n         `"""YYYY8888888PPPP"""\'')
+}
+
+const checkForEgg = (req, res) => {
+  if(req.route.path.indexOf("egg") != -1) {
+    makeEgg()
+  }
+}
+
+
 //routes and routing
 app.get('/', (req, res, next) => {
-   res.sendFile(path.join(__dirname + '/index.html'));
+  checkForEgg(req, res)
+  res.sendFile(path.join(__dirname + '/index.html'));
 })
 
 app.get('/see-our-chickens', (req, res, next) => {
+  checkForEgg(req, res)
   res.sendFile(path.join(__dirname + '/public' + '/see-our-chickens.html'));
 })
 
 app.get('/see-our-eggs', (req, res, next) => {
+  checkForEgg(req, res)
   res.sendFile(path.join(__dirname + '/public' + '/see-our-eggs.html'));
 })
 
 app.use((req, res) => {
-  res.send("Where do you think you're going?")
+  res.send('<img src="https://http.cat/404.jpg">')
 })
 
 
